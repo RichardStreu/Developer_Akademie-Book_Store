996119fe3302c887c6ea06e0775a9aeea4ae0d65
Richard Streu <rstreu13@icloud.com>
2024-09-04 18:23:29 +0200

    comments rendering ready

--- script.js
+++ script.js
@@ -10,40 +10,43 @@ function setDataToLocalStorage() {
   localStorage.setItem("books", JSON.stringify(books));
 }
 
+// initial rendering
+
+function initialRendering() {
+  getDataFromLocalStorage();
+
+  let bookWrapperRef = document.getElementById("bookWrapper");
+
+  for (let index = 0; index < books.length; index++) {
+    bookWrapperRef.innerHTML += renderBookContainerTemplate(index);
+    bookWrapperRef.innerHTML += getDividerTemplate();
+  }
+}
+
 // render templates
 
 function renderBookContainerTemplate(index) {
-  let bookBox;
-  return getBookContainerTemplate(index);
+  let commentContent = renderBookComments(index);
+  return getBookContainerTemplate(index, commentContent);
 }
 
 function renderBookComments(currentBookIndex) {
-  let bookCommentBox = "";
-  for (let commentIndex = 0; commentIndex < books[currentBookIndex].comments.   length; commentIndex++) {
+  let commentBoxLowerPart = "";
+  for (
+    let commentIndex = 0;
+    commentIndex < books[currentBookIndex].comments.length;
+    commentIndex++
+  ) {
     let commentAutor = books[currentBookIndex].comments[commentIndex].name;
-    bookCommentBox += getBookCommentsTemplate(commentAutor); 
+    let comment = books[currentBookIndex].comments[commentIndex].comment;
+    commentBoxLowerPart += getBookCommentsTemplate(commentAutor, comment);
   }
-  return bookCommentBox;
+  return commentBoxLowerPart;
 }
 
 function renderDividerTemplate() {
   getDividerTemplate();
 }
 
-// initial
-
-function initialRendering() {
-
-  getDataFromLocalStorage();
-
-  let bookWrapperRef = document.getElementById("bookWrapper");
-  for (let index = 0; index < books.length; index++) {
-    
-    bookWrapperRef.innerHTML += renderBookContainerTemplate(index);
-    bookWrapperRef.innerHTML += renderBookComments(index);
-    bookWrapperRef.innerHTML += getDividerTemplate();
-
-  }
-}
 
 initialRendering();
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,13 +1,31 @@
 
-function getBookContainerTemplate() {
-  return `<div class="book-container"></div>`;
+function getBookContainerTemplate(index, commentContent) {
+  return `<div class="book-container">
+            <div class="bookBox"></div>
+
+            <div class="commentContainer">
+              
+              <div class="commentBoxUpperPart">
+              <p><b>Bewerte jetzt dieses Buch</b></p>
+                <div class="commentInputArea">
+                  <input type="text" placeholder="Dein Name"></inpu>
+                  <input type="text" placeholder="Deine Bewertung"></inpu>
+                </div>
+                <button id="commentSendButton" class="comment-send-button">Bewertung abgeben</button>
+              </div>
+
+              <div class="commentBoxLowerPart">
+              ${commentContent}
+              </div>`;
 }
 
-function getBookCommentsTemplate(autor) {
-  return `<div class="commentBox"><p>${autor}</p></div>`;
+function getBookCommentsTemplate(autor, comment) {
+  return `<div class="commentContent">
+            <p><b>${autor}</b></p>
+            <p>${comment}</p>
+          </div>`;
 }
 
 function getDividerTemplate() {
   return `<div class="divider"></div>`;
 }
-
