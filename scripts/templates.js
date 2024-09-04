925bc535bb0ffd7924b6d8bc8a958d11820234a7
Richard Streu <rstreu13@icloud.com>
2024-09-04 20:15:47 +0200

    comment container basic styling ready

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,22 +1,27 @@
-
 function getBookContainerTemplate(index, commentContent) {
   return `<div class="book-container">
-            <div class="bookBox"></div>
+            <div class="bookBox">
+            
+            </div>
 
             <div class="commentContainer">
               
               <div class="commentBoxUpperPart">
-              <p><b>Bewerte jetzt dieses Buch</b></p>
+                <p><b>Bewerte jetzt dieses Buch</b></p>
                 <div class="commentInputArea">
-                  <input type="text" placeholder="Dein Name"></inpu>
-                  <input type="text" placeholder="Deine Bewertung"></inpu>
+                  <input class="input-name" type="text" placeholder="Dein Name"></inpu>
+                  <input class="input-comment" type="text" placeholder="Deine Bewertung"></inpu>
                 </div>
                 <button id="commentSendButton" class="comment-send-button">Bewertung abgeben</button>
               </div>
 
               <div class="commentBoxLowerPart">
               ${commentContent}
-              </div>`;
+              </div>
+
+            </div>
+
+          </div>`;
 }
 
 function getBookCommentsTemplate(autor, comment) {
